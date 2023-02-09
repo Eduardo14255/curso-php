aef515f024cdc9aa882106e218217040e20ecf6e
Eduardo noveli <eduardo.noveli001@outlook.com>
2023-02-08 22:06:48 -0300

    Segunda versão Excluir

--- db/excluir2.php
+++ db/excluir2.php
@@ -1,5 +1,4 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
 <div class="titulo">Excluir Registros 2</div>
 
 <?php
@@ -8,7 +7,14 @@ require_once "conexao.php";
 $conexao = novaConexao();
 $registros = [];
 
-$sql = "SELECT * FROM cadastro";
+if($_GET['excluir']){
+    $excluirSQL = "DELETE FROM cadastro WHERE id = ?";
+    $stmt = $conexao->prepare($excluirSQL);
+    $stmt->bind_param("i", $_GET['excluir']);
+    $stmt->execute();
+}
+
+$sql = "SELECT id, nome, nascimento, email FROM cadastro";
 $resultado = $conexao->query($sql);
 
 if ($resultado->num_rows > 0) {
@@ -23,24 +29,33 @@ $conexao->close();
 
 <table class="table table-hover table-striped table-bordered">
     <thead>
+        <th>ID</th>
         <th>Nome</th>
         <th>Email</th>
         <th>Nascimento</th>
+        <th>Ações</th>
     </thead>
 
     <tbody>
         <?php foreach ($registros as $registro) : ?>
             <tr>
+                <td><?= $registro['id'] ?></td>
                 <td><?= $registro['nome'] ?></td>
                 <td><?= $registro['email'] ?></td>
-                <td><?= $registro['nascimento'] ?></td>
+                <td>
+                    <?=
+                    date('d/m/Y', strtotime($registro['nascimento']))
+                    ?>
+                </td>
+                <td><a href="/curso-php/exercicio.php?dir=db&file=excluir2&excluir=<?= $registro['id'] ?>" class="btn btn-danger"> Excluir</a>
+                </td>
             </tr>
         <?php endforeach ?>
     </tbody>
 </table>
 
 <style>
-    table > * {
-        font-size:1.2rem ;
+    table>* {
+        font-size: 1.2rem;
     }
 </style>
